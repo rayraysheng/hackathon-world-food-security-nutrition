897ddba45cfcfc950ff474725c6b2887dce963ad
skliangsjsu <sarah.liang@sjsu.edu>
2024-03-31 10:37:03 -0700

    added css files and adjusted dashboard

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,7 +1,17 @@
+// Dashboard.js
+
 import React, { useState } from 'react';
 import Chatbot from '../components/Chatbot.js';
 import FloatingChatbotIcon from '../components/FloatingChatbotIcon';
 import Modal from 'react-modal';
+<<<<<<< Updated upstream
+=======
+import Annotation from '../components/Annotation';
+import GraphFilter from '../components/GraphFilter';
+import '../style/Dashboard.css';
+import Sidebar from '../components/Sidebar.js';
+import HeaderNavBar from '../components/HeaderNavBar.js'; // Import your header navigation bar component
+>>>>>>> Stashed changes
 
 Modal.setAppElement('#root'); // Avoid screen reader issues
 
@@ -12,12 +22,36 @@ function Dashboard() {
   const handleCloseChatbotModal = () => setIsChatbotModalOpen(false);
 
   return (
+<<<<<<< Updated upstream
     <div>
       <h2>Dashboard</h2>
       {/* Other dashboard content */}
 
+=======
+    <div className="dashboard-container">
+      {/* Header Navigation Bar */}
+      <HeaderNavBar />
+      
+      {/* Middle Content */}
+      <div className="middle-content">
+        <div className="sidebar">
+          <Sidebar />
+        </div>
+        <div className="graphfilter-container">
+          <GraphFilter />
+        </div>
+        <div>
+          <Annotation content="This is an annotation!" position="top">
+            {/* Content to be annotated */}
+          </Annotation>
+        </div>
+      </div>
+
+      {/* Floating Chatbot Icon */}
+>>>>>>> Stashed changes
       <FloatingChatbotIcon onClick={handleOpenChatbotModal} />
 
+      {/* Chatbot Modal */}
       <Modal
         isOpen={isChatbotModalOpen}
         onRequestClose={handleCloseChatbotModal}
