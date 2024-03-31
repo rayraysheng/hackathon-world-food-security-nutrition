3b696857fbcd3359e518d2c988764eb7c8f3112f
Sarah Mai <sarahmai@Sarahs-Air-2.hsd1.ca.comcast.net>
2024-03-31 12:07:30 -0700

    working temporary annotations pop-up

--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -13,6 +13,10 @@ function Dashboard() {
   const handleOpenChatbotModal = () => setIsChatbotModalOpen(true);
   const handleCloseChatbotModal = () => setIsChatbotModalOpen(false);
 
+  const [isAnnotationModalOpen, setIsAnnotationModalOpen] = useState(false);
+  const handleOpenAnnotationModal = () => setIsAnnotationModalOpen(true);
+  const handleCloseAnnotationModal = () => setIsAnnotationModalOpen(false);
+
   return (
     <div>
       <h2>Dashboard</h2>
@@ -21,10 +25,23 @@ function Dashboard() {
       <GraphFilter />
 
       <div>
-        <Annotation content="This is an annotation!" position="top">
-          {/* Content to be annotated */}
-        </Annotation>
+        {/* Button to open the annotation modal */}
+        <button onClick={handleOpenAnnotationModal}>Add Annotation</button>
       </div>
+
+      {/* Annotation Modal */}
+      <Modal
+        isOpen={isAnnotationModalOpen}
+        onRequestClose={handleCloseAnnotationModal}
+        contentLabel="Annotation Modal"
+        className="annotation-modal"
+        overlayClassName="annotation-modal-overlay"
+      >
+        {/* Content inside the annotation modal */}
+        <Annotation content="This is an annotation!" position="top">
+        </Annotation>
+        <button onClick={handleCloseAnnotationModal}>Close</button>
+      </Modal>
 
       <FloatingChatbotIcon onClick={handleOpenChatbotModal} />
 
